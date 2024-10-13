3a46a189159f625559706f09fbba61368eee755c
Mishal Yadav <84717734+Mishu-techAWS@users.noreply.github.com>
2024-10-13 09:49:29 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,14 +1,14 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-001"
-  project                  = "tt-dev-001"
+  name                     = "tt-githubdemo-bucket-000001"
+  project                  = "test-terraform-438504"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
 #
 resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-002"
-  project                  = "tt-dev-001"
+  name                     = "tt-githubdemo-bucket-000002"
+  project                  = "test-terraform-438504"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
