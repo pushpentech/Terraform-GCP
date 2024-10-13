366f0df87ead3abfc942a11b5c7a09ee93f0f8c5
pushpentech <141352486+pushpentech@users.noreply.github.com>
2024-10-13 20:15:37 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -5,4 +5,10 @@ resource "google_storage_bucket" "my-bucket" {
   force_destroy            = true
   public_access_prevention = "enforced"
 }
-### dummy
+
+resource "google_storage_bucket" "my-bucket3" {
+  name                     = "pushp2"
+  project                  = "test-terraform-438504"
+  location                 = "US"
+  force_destroy            = true
+  public_access_prevention = "enforced"
