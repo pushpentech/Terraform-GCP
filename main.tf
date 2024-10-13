a6b4e3a6c58eeeff4242fa5920057156e05aa54a
pushpentech <141352486+pushpentech@users.noreply.github.com>
2024-10-13 14:31:49 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -1,17 +1,8 @@
 resource "google_storage_bucket" "my-bucket" {
-  name                     = "tt-githubdemo-bucket-000001"
+  name                     = "pushp1"
   project                  = "test-terraform-438504"
   location                 = "US"
   force_destroy            = true
   public_access_prevention = "enforced"
 }
-#
-resource "google_storage_bucket" "my-bucket2" {
-  name                     = "tt-githubdemo-bucket-000002"
-  project                  = "test-terraform-438504"
-  location                 = "US"
-  force_destroy            = true
-  public_access_prevention = "enforced"
-}
-#
 ### dummy
